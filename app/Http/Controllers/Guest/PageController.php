27f6ab18d9da13d03359753c2ef03af41cedfc23
GiuliaSetti <giulia.setti96@gmail.com>
2023-05-11 18:44:18 +0200

    feat: added use model and prepared db

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -11,6 +12,8 @@ class PageController extends Controller
 
     public function index(){
 
-        return view('home');
+        $trains = Train::all();
+
+        return view('home', compact('trains'));
     }
 }
