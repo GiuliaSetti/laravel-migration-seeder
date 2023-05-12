1b4b701972a9cb62da7d1fac5faab6369c147ddd
GiuliaSetti <giulia.setti96@gmail.com>
2023-05-12 19:06:58 +0200

    feat: printed trains leaving from now on

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use DateTime;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -12,7 +13,11 @@ class PageController extends Controller
 
     public function index(){
 
-        $trains = Train::all();
+        // $trains = Train::all();
+
+        $departure = new DateTime();
+
+        $trains = Train::where('departure', '>=', $departure->format('Y-m-d H:i:s'))->get();
 
         return view('home', compact('trains'));
     }
